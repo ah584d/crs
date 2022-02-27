37ab3b870a3a0e54f8ee92bc4966ddc0971fcc09
Hamu, Avraham (ah584d) <ah584d@intl.att.com>
2022-02-28 00:16:38 +0200

    adding network services

---
+++ stkOverflow/src/services/logic.ts
@@ -0,0 +1,16 @@
+import { apiRoutes } from '../config/apiRoutes';
+import { fetchData } from './network';
+
+export const getUserInfo = async (uid: string, params?: Record<string, string>): Promise<any | null> => {
+  uid = '1264804';
+  const url = buildUrl(uid, params);
+  const [result, error] = await fetchData(url);
+  if (error) {
+    // raise up error message
+    return null;
+  } else {
+    return result;
+  }
+};
+
+export const buildUrl = (uid: string, params?: Record<string, string>): string => `${apiRoutes.STK_OVERFLOW.PROFILE}/${uid}/questions?order=desc&sort=activity&site=stackoverflow`;
--- stkOverflow/src/services/network.ts
+++ stkOverflow/src/services/network.ts
@@ -1,1 +1,17 @@
-export const myFecth = () => undefined;
+export const fetchData = async (url: string): Promise<[Record<string, any> | null, unknown | null]> => {
+  console.log(`url: ${url}`);
+  try {
+    const result = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return [result.json(), null];
+  } catch (error) {
+    console.log(`Error occured while fetching user info`);
+    return [null, error];
+  }
+};
